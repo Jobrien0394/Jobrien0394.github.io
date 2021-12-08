5f35d2cbe1e1efa2b2cd55ea648ca4d6d434b44a
John OBrien <j.obrien0394@gmail.com>
2021-12-08 08:56:07 -0500

    Removed irrelevant comment.

--- code.js
+++ code.js
@@ -1,6 +1,3 @@
-// To run this assignment, right click on index.html in the Visual Studio Code file explorer to the left
-// and select "Open with Live Server"
-
 // my global variables
 let score = 0
 let categories = []
